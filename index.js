f21b2d96fca4adfdbbdf8d988ed5889a5247374a
James Chalk <jmmchalk@gmail.com>
2019-10-14 18:51:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+"Hello" + specialGuest + "!";
 
 var topic = "space";
 
